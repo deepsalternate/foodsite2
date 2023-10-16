d1621ce05986636ee3d88c9d2bd53c0280383fb1
unknown <hello@example.com>
2023-10-16 17:25:04 +0530

    changes made to program in view py to help with seing food items

--- food/views.py
+++ food/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render ,HttpResponse
+from .models import Item
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('Hello World')
+    item_list=Item.objects.all()
+    
+    return HttpResponse(item_list)
 def item(request):
     return HttpResponse('this is an item view')
 

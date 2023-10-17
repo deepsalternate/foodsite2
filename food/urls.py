c196ca43e68bee46394719fd16df4101c6bc33ee
unknown <hello@example.com>
2023-10-17 17:36:13 +0530

    added templates and done some staticfiles remove some hardcoded urls

--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,12 @@
 from .import views 
 from django.urls  import path
-
+app_name='food'
 urlpatterns = [
+    #/food/
     path('',views.hello,name='index'),
+    #/food/item
     path('item/',views.item),
+    #food/item_id    could be any key 1,2 ,3
+    path('<int:item_id>/',views.detail,name="detail"),
+    
 ]
--- food/views.py
+++ food/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render ,HttpResponse
-from .models import Item
+from .models import Item 
+from django.template import loader
 
 # Create your views here.
 def hello(request):
     item_list=Item.objects.all()
-    
-    return HttpResponse(item_list)
+    # template=loader.get_template('food/index.html')
+    context={
+        'item_list':item_list,
+    }
+    return render(request,'food/index.html',context)
+    #  " does the same as above "return HttpResponse(template.render(context,request))
 def item(request):
     return HttpResponse('this is an item view')
 
+def detail(request,item_id):
+    item=Item.objects.get(pk=item_id)
+    context={
+        'item':item,
+    }
+    # return HttpResponse ("this is an item view : %s" % item_id )
+    return render(request,'food/detail.html',context)
